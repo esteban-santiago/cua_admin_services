dfca76674ed72ed456d119786c950544d92c84fe
esteban-santiago <esteban.santiago@gmail.com>
2016-10-12 12:09:51 -0300

    Update Address.java

--- src/main/java/com/cua/admin/entities/Address.java
+++ src/main/java/com/cua/admin/entities/Address.java
@@ -10,6 +10,7 @@ import javax.persistence.Entity;
 import javax.persistence.ForeignKey;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 /**
