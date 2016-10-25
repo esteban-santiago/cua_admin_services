f2b0e0f69769b9462111caf39ca1841db294a10a
esteban_santiago <esteban_santiago@192.168.0.13>
2016-10-25 01:23:26 -0300

    Commit 01:23

--- src/test/java/com/cua/admin/tests/entities/EmployeeTests.java
+++ src/test/java/com/cua/admin/tests/entities/EmployeeTests.java
@@ -13,7 +13,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.util.Assert;
 import com.cua.admin.repositories.ActivityRepository;
 import com.cua.admin.repositories.CategoryRepository;
 import com.cua.admin.repositories.NationalityRepository;
