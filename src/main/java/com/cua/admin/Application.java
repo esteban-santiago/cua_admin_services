76729a4b50aa8e12234b86f8faf4fdc39277957a
esteban_santiago <esteban_santiago@192.168.0.13>
2016-10-25 20:21:38 -0300

    Commit 20:21

--- src/main/java/com/cua/admin/Application.java
+++ src/main/java/com/cua/admin/Application.java
@@ -3,9 +3,12 @@ package com.cua.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+
+
 public class Application {
 
 	public static void main(String[] args) {
