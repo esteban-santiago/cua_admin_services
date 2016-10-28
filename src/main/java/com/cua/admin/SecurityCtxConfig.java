b2c11b9d4bc822742657acbcec56ba9080e36388
esteban_santiago <esteban_santiago@192.168.0.13>
2016-10-27 23:01:50 -0300

    Commit unsecure /api/

--- src/main/java/com/cua/admin/SecurityCtxConfig.java
+++ src/main/java/com/cua/admin/SecurityCtxConfig.java
@@ -6,9 +6,11 @@
 package com.cua.admin;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 /**
  *
@@ -23,13 +25,18 @@ public class SecurityCtxConfig extends WebSecurityConfigurerAdapter {
      * @param http
      * @throws Exception
      */
-    //@Override
-    public void reconfigure(HttpSecurity http) throws Exception {
-     http.antMatcher("/api/login")
-        .csrf().disable()
-        .authorizeRequests()
-        .antMatchers("/api/login")
-        .permitAll();
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+            .authorizeRequests()
+            .antMatchers(HttpMethod.GET, "/api/**").authenticated()
+            .antMatchers(HttpMethod.POST, "/api/**").authenticated()
+            .antMatchers(HttpMethod.PUT, "/api/**").authenticated()
+            .antMatchers(HttpMethod.DELETE, "/api/**").authenticated()
+            .anyRequest().permitAll()
+            .and()
+            .httpBasic().and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
     
 }
