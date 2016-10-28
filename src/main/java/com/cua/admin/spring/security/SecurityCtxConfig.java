efbf7d2a3db1a013cf9cec6a49bf5cd642dedb8d
esteban_santiago <esteban_santiago@192.168.0.13>
2016-10-27 23:26:17 -0300

    Commit unsecure /api/ & new package

--- src/main/java/com/cua/admin/spring/security/SecurityCtxConfig.java
+++ src/main/java/com/cua/admin/spring/security/SecurityCtxConfig.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.cua.admin;
+package com.cua.admin.spring.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
