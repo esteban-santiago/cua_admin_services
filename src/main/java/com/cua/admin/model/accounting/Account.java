541c65b67f0df07650daed8e93c5c103dac17bcb
esteban_santiago <esteban_santiago@192.168.0.13>
2016-10-31 00:55:03 -0300

    Commit 00:54

--- src/main/java/com/cua/admin/model/accounting/Account.java
+++ src/main/java/com/cua/admin/model/accounting/Account.java
@@ -5,10 +5,31 @@
  */
 package com.cua.admin.model.accounting;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.ToString;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.GenericGenerator;
+
 /**
  *
  * @author esteban_santiago
  */
-public class Account {
+
+@ToString
+@EqualsAndHashCode
+@Entity
+@Table(schema="nextg", name="account")
+public class Account implements Serializable {
+    @Id
+    private Integer id;
+    private Integer firstOrderGrouping;
+    private Integer secondOrderGrouping;
+    private Integer thirdOrderGrouping;
+    private String description;
     
 }
---
+++ src/main/java/com/cua/admin/model/accounting/AccountingPlan.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cua.admin.model.accounting;
+
+import java.util.Set;
+
+/**
+ *
+ * @author esteban_santiago
+ */
+public class AccountingPlan {
+    private Set<Account> accounts;
+}
