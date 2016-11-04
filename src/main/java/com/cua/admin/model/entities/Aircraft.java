8f6025cf825999131a660e952c524c603bbaaf9c
esteban_santiago <esteban_santiago@192.168.0.13>
2016-11-04 00:58:52 -0300

    Commit 00:58

--- src/main/java/com/cua/admin/model/entities/Aircraft.java
+++ src/main/java/com/cua/admin/model/entities/Aircraft.java
@@ -9,8 +9,6 @@ import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
 import javax.persistence.Table;
 import lombok.ToString;
 import lombok.EqualsAndHashCode;
@@ -25,13 +23,13 @@ import org.hibernate.annotations.Parameter;
 @ToString
 @EqualsAndHashCode
 @Entity
-@Table(name="plane")
+@Table(name="aircraft")
 public class Aircraft implements Serializable {
  @GenericGenerator(
         name = "SequenceGenerator",
         strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
         parameters = {
-                @Parameter(name = "sequence_name", value = "plane_id_seq"),
+                @Parameter(name = "sequence_name", value = "aircraft_id_seq"),
                 @Parameter(name = "initial_value", value = "1000"),
                 @Parameter(name = "increment_size", value = "1")
         }
@@ -42,4 +40,60 @@ public class Aircraft implements Serializable {
     private String registration; //Matrícula
     private String model; //Modelo
     private String status; //Status: Activo, Inactivo 
+
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the registration
+     */
+    public String getRegistration() {
+        return registration;
+    }
+
+    /**
+     * @param registration the registration to set
+     */
+    public void setRegistration(String registration) {
+        this.registration = registration;
+    }
+
+    /**
+     * @return the model
+     */
+    public String getModel() {
+        return model;
+    }
+
+    /**
+     * @param model the model to set
+     */
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    /**
+     * @return the status
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
---
+++ src/main/java/com/cua/admin/model/entities/Airfield.java
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cua.admin.model.entities;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+/**
+ *
+ * @author esteban_santiago
+ */
+@ToString
+@EqualsAndHashCode
+@Entity
+@Table(name="airfield")
+public class Airfield implements Serializable {
+    @GenericGenerator(
+        name = "SequenceGenerator",
+        strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+        parameters = {
+                @Parameter(name = "sequence_name", value = "airfield_id_seq"),
+                @Parameter(name = "initial_value", value = "1000"),
+                @Parameter(name = "increment_size", value = "1")
+        }
+    )
+    @GeneratedValue(generator = "SequenceGenerator")    
+    @Id
+    private Integer id;
+    @Column(name = "identification_anac")
+    private String identificationANAC;
+    @Column(name = "identification_oaci")
+    private String identificationOACI;
+    private String name;
+    private String latitude;
+    private String longitude;
+
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the identificationANAC
+     */
+    public String getIdentificationANAC() {
+        return identificationANAC;
+    }
+
+    /**
+     * @param identificationANAC the identificationANAC to set
+     */
+    public void setIdentificationANAC(String identificationANAC) {
+        this.identificationANAC = identificationANAC;
+    }
+
+    /**
+     * @return the identificationOACI
+     */
+    public String getIdentificationOACI() {
+        return identificationOACI;
+    }
+
+    /**
+     * @param identificationOACI the identificationOACI to set
+     */
+    public void setIdentificationOACI(String identificationOACI) {
+        this.identificationOACI = identificationOACI;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the latitude
+     */
+    public String getLatitude() {
+        return latitude;
+    }
+
+    /**
+     * @param latitude the latitude to set
+     */
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    /**
+     * @return the longitude
+     */
+    public String getLongitude() {
+        return longitude;
+    }
+
+    /**
+     * @param longitude the longitude to set
+     */
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+    
+}
