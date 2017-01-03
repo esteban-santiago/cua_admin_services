6c5d8aaa9ff51c177e419d20c58db0f181c81780
esantiago <esantiago@9.48.67.217>
2017-01-03 19:08:15 -0300

    Commit FlightRecord

--- src/main/java/com/cua/admin/model/core/flight/FlightRecord.java
+++ src/main/java/com/cua/admin/model/core/flight/FlightRecord.java
@@ -1,19 +1,15 @@
 package com.cua.admin.model.core.flight;
 
-import com.cua.admin.model.core.Member;
 import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.io.Serializable;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
 import javax.persistence.CascadeType;
 import javax.persistence.FetchType;
 import javax.persistence.ForeignKey;
