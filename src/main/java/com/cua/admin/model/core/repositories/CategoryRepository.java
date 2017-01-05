64074e720266f120fbe1051f47271b2549f5ed1e
esantiago <esantiago@9.48.67.217>
2017-01-05 17:37:18 -0300

    Commit @Repository

--- src/main/java/com/cua/admin/model/core/repositories/CategoryRepository.java
+++ src/main/java/com/cua/admin/model/core/repositories/CategoryRepository.java
@@ -4,7 +4,9 @@ import com.cua.admin.model.core.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
     List<Category> findByDescription(String description);
--- src/test/java/com/cua/admin/tests/core/SpringIntegrationTest.java
+++ src/test/java/com/cua/admin/tests/core/SpringIntegrationTest.java
@@ -1,12 +1,12 @@
 package com.cua.admin.tests.core;
 
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@ActiveProfiles("dev")
+@ActiveProfiles("test")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 
