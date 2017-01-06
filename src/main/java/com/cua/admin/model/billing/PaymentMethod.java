bc29de3f4266230e08aa90257acea0df9b293829
esteban_santiago <esteban_santiago@192.168.0.13>
2017-01-06 00:22:21 -0300

    Commit Payment

--- src/main/java/com/cua/admin/model/billing/PaymentMethod.java
+++ src/main/java/com/cua/admin/model/billing/PaymentMethod.java
@@ -1,19 +1,36 @@
 package com.cua.admin.model.billing;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import java.io.Serializable;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Table;
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 
-@Getter
-@RequiredArgsConstructor
-
-public enum PaymentMethod {
-    CASH("Efectivo"),
-    CREDIT_CARD("Tarjeta de Crédito"), 
-    DEBIT_CARD("Tarjeta de Débito"), 
-    CHECK("Cheque"),
-    BANK_ACCOUNT("Cuenta Corriente $"),
-    @Deprecated
-    HOURS_ACCOUNT("Cuenta Corriente Hs");
-    
-    private final String description;
+@Data
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "payment_type", discriminatorType = DiscriminatorType.STRING)
+@Table(name = "payment_method")
+public abstract class PaymentMethod implements Serializable {
+    @GenericGenerator(
+            name = "SequenceGenerator",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                @Parameter(name = "sequence_name", value = "payment_method_id_seq"),
+                @Parameter(name = "initial_value", value = "1000"),
+                @Parameter(name = "increment_size", value = "1")
+            }
+    )
+    @GeneratedValue(generator = "SequenceGenerator")
+    @Id
+    private Integer id;
+//    pr
 }
--- src/test/java/com/cua/admin/tests/core/SpringIntegrationTest.java
+++ src/test/java/com/cua/admin/tests/core/SpringIntegrationTest.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@ActiveProfiles("test")
+@ActiveProfiles("dev")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 
