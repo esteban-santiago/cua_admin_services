0ca08be095d850d442496739c0407347b55e99b9
esteban_santiago <esteban_santiago@192.168.0.13>
2017-01-27 19:41:19 -0300

    Commit Angular

--- src/main/java/com/cua/admin/security/CorsConfiguration.java
+++ src/main/java/com/cua/admin/security/CorsConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class CorsConfiguration extends WebMvcConfigurerAdapter {
      @Override
      public void addCorsMappings(CorsRegistry registry) {
-         registry.addMapping("/**");
+         registry.addMapping("/**")
+                 .allowedOrigins("http://192.168.0.13:8383");
      }
 }
