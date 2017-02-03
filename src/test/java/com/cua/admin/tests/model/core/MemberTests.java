c84b69a953d84b84fab1ee63737788249468351b
esteban_santiago <esteban_santiago@192.168.0.13>
2017-02-02 21:51:37 -0300

    Commit Reingeniería de la Persona

--- src/test/java/com/cua/admin/tests/model/core/MemberTests.java
+++ src/test/java/com/cua/admin/tests/model/core/MemberTests.java
@@ -98,6 +98,5 @@ public class MemberTests extends SpringIntegrationTest {
                 .stream().forEach(
                         aRecord -> System.out.println(aRecord)
                 );
-    }
-    
+    }   
 }
