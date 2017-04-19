acfb3862cf98d11dc904549c775eab6e4490a506
esteban_santiago <esteban_santiago@192.168.0.13>
2017-04-18 22:40:58 -0300

    Commit Update

--- src/main/java/com/cua/admin/model/finance/billing/Payment.java
+++ src/main/java/com/cua/admin/model/finance/billing/Payment.java
@@ -28,10 +28,8 @@ public class Payment implements Serializable {
             name = "SequenceGenerator",
             strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
             parameters = {
-                @Parameter(name = "sequence_name", value = "payment_id_seq")
-                ,
-                @Parameter(name = "initial_value", value = "1")
-                ,
+                @Parameter(name = "sequence_name", value = "payment_id_seq"),
+                @Parameter(name = "initial_value", value = "1"),
                 @Parameter(name = "increment_size", value = "1")
             }
     )
---
+++ src/main/java/com/cua/admin/model/finance/billing/Promotion.java
@@ -0,0 +1,39 @@
+package com.cua.admin.model.finance.billing;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+/**
+ *
+ * @author esteban_santiago
+ */
+@Data
+@Entity
+@Table(name = "promotion")
+public class Promotion implements Serializable {
+    @GenericGenerator(
+            name = "SequenceGenerator",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                @Parameter(name = "sequence_name", value = "promotion_id_seq"),
+                @Parameter(name = "initial_value", value = "1"),
+                @Parameter(name = "increment_size", value = "1")
+            }
+    )
+    @GeneratedValue(generator = "SequenceGenerator")
+    @Id
+    private Integer id;
+    
+    private String code;
+    
+    private String description;
+    
+    private float discount;
+    
+}
